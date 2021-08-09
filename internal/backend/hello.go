77afe4c572c268720bf5972da88336baafb46c57
jasonlimantoro <jason@ewburgers.com>
2021-08-09 11:15:26 +0800

    use mapstructure

--- internal/backend/hello.go
+++ internal/backend/hello.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 
 	"github.com/jasonlimantoro/hello-microservice/pkg"
+	"github.com/mitchellh/mapstructure"
 )
 
 func CreateHandlerHello(svcCtx HelloServiceContext) http.HandlerFunc {
 	return pkg.CreateHandler(svcCtx, pkg.DecodeJSON, pkg.EncodeJSON, Hello)
 }
 func Hello(ctx context.Context, request interface{}) interface{} {
-	req := request.(helloRequest)
+	var req helloRequest
+	mapstructure.Decode(request, &req)
 	svcCtx := ctx.Value(pkg.SvcCtxKey).(HelloServiceContext)
 	return helloResponse{Message: svcCtx.helloworldClient.SayHello(ctx, req.Name)}
 }
